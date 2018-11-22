3fb1b4b81573a8dfc737f0c893c03e3976a1ac05
Marcos <mallth194@gmail.com>
2018-11-22 16:02:28 -0400

    Primer commit - rest server

---
+++ controllers/mensajes.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from 'express';
+
+export function get(req:Request, res:Response) {
+
+    res.json({
+        ok: true,
+        mensaje: 'Todo esta bien!!',
+    });
+}
+
+export function post(req:Request, res:Response) {
+
+    const cuerpo = req.body.cuerpo;
+    const de = req.body.de;
+    
+    res.json({
+        ok: true,
+        mensaje: 'Todo esta bien!!',
+        cuerpo,
+        de
+    });
+}
+
+export function mensajesConId(req:Request, res:Response) {
+
+    const cuerpo = req.body.cuerpo;
+    const de = req.body.de;
+    const id = req.params.id;
+    
+    res.json({
+        ok: true,
+        mensaje: 'Todo esta bien!!',
+        cuerpo,
+        de,
+        id
+    });
+}
---
+++ routes/router.ts
@@ -0,0 +1,26 @@
+import { Router, Request, Response } from 'express';
+import { get, post, mensajesConId } from '../controllers/mensajes';
+
+const router = Router();
+
+router.get('/mensajes', get);
+router.post('/mensajes', post);
+router.post('/mensajes/:id', mensajesConId);
+
+// router.get('/mensajes', (req: Request, res: Response) => {
+
+//     res.json({
+//         ok: true,
+//         mensaje: 'Todo esta bien!!'
+//     });
+// });
+
+// router.post('/mensajes', (req: Request, res: Response) => {
+
+//     res.json({
+//         ok: true,
+//         mensaje: 'POST - Listo!!'
+//     });
+// });
+
+export default router;
